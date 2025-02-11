69acd9e95a84924c84bae178f88199d741221c96
JEVizcaya <jevizcaya@hotmail.com>
2025-02-11 20:13:47 +0100

    datos film

--- peliculas/all.php
+++ peliculas/all.php
@@ -1,6 +1,6 @@
 <?php
-include("conexiondb.php");
-$sql = "select actor_id, first_name, last_name from actor";
+include("../conexiondb.php");
+$sql = "select title, description,release_year from film";
 $result = $conexion->query($sql);
 $datos = $result->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($datos);
