89fd5cc45e3b77d1612fbd49597865f94cd224d6
JEVizcaya <jevizcaya@hotmail.com>
2025-02-12 16:53:21 +0100

    peliculas actors

--- actores/actor.php
+++ actores/actor.php
@@ -1,5 +1,5 @@
 <?php
-include("conexiondb.php");
+include("../conexiondb.php");
 $sql="select actor_id,first_name,last_name from actor where actor_id=:actor_id";
 $stm=$conexion->prepare($sql);
 $stm->bindParam(":actor_id",$_GET['actor_id']);
