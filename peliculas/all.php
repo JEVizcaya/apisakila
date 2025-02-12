3c1b0db04e6ecd2dda1b60f12b9a83034dce7bff
JEVizcaya <jevizcaya@hotmail.com>
2025-02-12 19:13:14 +0100

    acceso dominios externos

--- peliculas/all.php
+++ peliculas/all.php
@@ -4,7 +4,7 @@ $sql = "select title, description,release_year from film";
 $result = $conexion->query($sql);
 $datos = $result->fetchAll(PDO::FETCH_ASSOC);
 $json = json_encode($datos);
-
+header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 echo $json;
 
