50048e2d626dcf78c4b7dc05757e2d246d3d10fb
JEVizcaya <jevizcaya@hotmail.com>
2025-02-13 17:41:49 +0100

    generar token

--- config_sample.php
+++ config_sample.php
@@ -3,4 +3,6 @@ define("HOST", "direccion del servidor");
 define("DB_USER","usuario bbdd");
 define("DB_PASS","password bbdd");
 define("DB_DATABASE","ddbb");
+define("JWT_SECRET_KEY","mi_contraseña_super_secreta");
+?>
 ?>
